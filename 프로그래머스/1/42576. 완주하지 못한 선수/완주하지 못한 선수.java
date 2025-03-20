e82bceb9c91bae469a00e152773bb50e133e5e4c
dohyeon <127070191+dohyeon321@users.noreply.github.com>
2025-03-20 23:26:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.61 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String p : participant) {
+            int n = 0;
+            if(map.containsKey(p)){
+                n = map.get(p);
+                map.put(p, ++n);
+            }
+            map.put(p,n);
+        }
+        
+        for(String p : completion) {
+            if(map.containsKey(p)){
+                int n = map.get(p);
+                map.put(p, --n);
+
+            }
+        }
+                
+        for(String p: participant) {
+            if(map.get(p) == 0) answer=p;
+        }
+        
+        
+        return answer;
+    }
+}
